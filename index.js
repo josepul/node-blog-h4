058e0a1fe647629192384bb3ff8ad3ab32b4f295
Jose <josepul@gmail.com>
2017-11-22 19:58:53 +0100

    Commit inicial

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req , res ) => res.send('Hello World'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'));
